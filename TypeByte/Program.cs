eb6e98db54c38b3c2323c3d7710788ad8d97f3b8
leandra-oliveira1234 <115712205+leandra-oliveira1234@users.noreply.github.com>
2022-10-27 12:10:00 -0300

    Add files via upload

---
+++ TypeByte/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TypeByte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           try
+           {
+                byte x = Convert.ToByte(Console.ReadLine());
+                Console.WriteLine("os valores digitos são: {0}", x);
+           }
+            catch (OverflowException)
+           {
+
+                Console.WriteLine("o valor digitado esta fora do maximo, o valor maximo é 255");
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("vocé digitou um texto. Só é aceitavel  um numero de 0 a 255");
+            }
+          
+         
+
+
+
+        }
+    }
+}
